011918f93661aeb35155f687b0c7c3709edce635
soulomoon <fwy996602672@gmail.com>
2018-08-01 17:58:11 +0800

    refactor Kyu3.AlphabeticAnagrams.hs

--- src/Kyu3/AlphabeticAnagrams.hs
+++ src/Kyu3/AlphabeticAnagrams.hs
@@ -1,13 +1,9 @@
 module Kyu3.AlphabeticAnagrams where 
 import Data.List
 
-factorial :: Int -> Integer
-factorial = product . flip take [1 ..]
-
 lexiPos :: String -> Integer
-lexiPos lst@(x:xs) = lexiPos xs + first * div (factorial $ length xs) (repeatedNum lst)
-    where first = fromIntegral $ head $ elemIndices x $ sort lst
+lexiPos lst@(x:xs) = lexiPos xs + first * div (factorial $ length xs) repeatedNum
+    where first = fromIntegral . head . elemIndices x . sort $ lst
+          factorial = product . flip take [1 ..]
+          repeatedNum = product (map (\x -> fromIntegral $ factorial (length $ filter (==x) lst)) $ nub lst)
 lexiPos _ = 1
-
-repeatedNum :: String -> Integer
-repeatedNum lst = product (map (\x -> fromIntegral $ factorial (length $ filter (==x) lst)) $ nub lst)
